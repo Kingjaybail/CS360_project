d7595808dff7551f704bf0ed5e50d30e234b05a1
Connor-Debus <Connor.debusschere122@topper.wku.edu>
2025-08-21 10:22:22 -0500

    test

---
+++ app/services/processor.py
@@ -0,0 +1,9 @@
+# app/services/processor.py 
+
+def compute_sum(a: int, b: int) -> int:
+    """Compute the sum of two integers."""
+    return a + b
+
+def reverse_string(s: str) -> str:
+    """Reverse a string."""
+    return s[::-1] 
